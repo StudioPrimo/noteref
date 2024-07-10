1b2442e0452b43053ff63092ac6062112ebc26b8
sugimoto_e <eikun1014@gmail.com>
2024-07-10 22:40:32 +0900

    feat(front): add link of each page

---
+++ frontend/src/app/upload/page.tsx
@@ -0,0 +1,7 @@
+export default function Upload() {
+  return (
+    <>
+      <h1>Upload</h1>
+    </>
+  );
+}
--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,14 +1,41 @@
-import { Center, Flex, Image, Text } from '@chakra-ui/react';
-
+import { Center, Flex, Image, Link, Text } from '@chakra-ui/react';
 export default function Header() {
+  const pages = ['home', 'about', 'upload'];
+
   return (
     <>
-      <Flex direction={'row'} gap={5}>
-        <Image src="icon.svg" alt="icon" boxSize={'80px'} borderRadius="full" />
+      <Flex direction={'row'} gap={5} justify={'space-around'}>
+        <Image
+          src="icon.svg"
+          alt="icon"
+          boxSize={'80px'}
+          borderRadius="full"
+          mt={2}
+        />
         <Center>
-          <Text fontSize="3xl">
-            <Text as="i"> note refugee</Text>
+          <Text fontSize="3xl" as="i">
+            note refugee
           </Text>
+        </Center>
+        <Center>
+          {pages.map((page) => {
+            if (page === 'home') {
+              return (
+                <Link key={page} href="/" pr={2}>
+                  <Text fontSize="xl" as="i" _hover={{ cursor: 'pointer' }}>
+                    {page}
+                  </Text>
+                </Link>
+              );
+            }
+            return (
+              <Link key={page} href={`/${page}`} pr={2}>
+                <Text fontSize="xl" as="i" _hover={{ cursor: 'pointer' }}>
+                  {page}
+                </Text>
+              </Link>
+            );
+          })}
         </Center>
       </Flex>
     </>
