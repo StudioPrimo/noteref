4a8cf03ab89752ac0bf5bb7c82f23fb56113a85b
sugimoto_e <eikun1014@gmail.com>
2024-07-10 23:02:07 +0900

    feat(front): add scroll into layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
               <GridItem pl="2" bg="pink.300" area={'nav'}>
                 Nav
               </GridItem>
-              <GridItem pl="2" bg="green.300" area={'main'}>
+              <GridItem pl="2" bg="green.300" area={'main'} overflowY={'auto'}>
                 {children}
               </GridItem>
               <GridItem pl="2" bg="blue.300" area={'footer'}>
