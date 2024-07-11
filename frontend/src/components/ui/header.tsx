43f8a5388703cd58c6497481704d858b486921d2
r-dTakahi <r.d.tsuchitori0723@gmail.com>
2024-07-11 23:31:03 +0900

    feat(header): add navigation menu such as Home, About, Want

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,15 +1,33 @@
-import { Center, Flex, Image, Text } from '@chakra-ui/react';
+import { Center, Flex, Image, Text, Link, Box } from '@chakra-ui/react';
 
 export default function Header() {
   return (
     <>
-      <Flex direction={'row'} gap={5}>
-        <Image src="icon.svg" alt="icon" boxSize={'80px'} borderRadius="full" />
+      <Flex direction={'row'} align="center" gap={5} px={4}>
+        <Image
+          mt={1.5}
+          src="icon.svg"
+          alt="icon"
+          boxSize={'80px'}
+          borderRadius="full"
+        />
         <Center>
           <Text fontSize="3xl">
             <Text as="i"> note refugee</Text>
           </Text>
         </Center>
+        <Box ml="auto" />
+        <Flex direction={'row'} gap={5}>
+          <Link href="/" fontSize="xl">
+            Home
+          </Link>
+          <Link href="/about" fontSize="xl">
+            About
+          </Link>
+          <Link href="/want" fontSize="xl">
+            Want
+          </Link>
+        </Flex>
       </Flex>
     </>
   );
