937efcba6ea201c9b85df8bca0a27435d476e9bc
r-dTakahi <r.d.tsuchitori0723@gmail.com>
2024-07-12 23:58:39 +0900

    feat(viewPDF): add function for viewing pdf

--- frontend/src/next.config.mjs
+++ frontend/src/next.config.mjs
@@ -5,7 +5,8 @@ const nextConfig = {
     config.watchOptions = {   
       poll: 1000,             
       aggregateTimeout: 300   
-    }                         
+    }
+    config.resolve.alias.canvas = false;
     return config;            
   }                           
 };
